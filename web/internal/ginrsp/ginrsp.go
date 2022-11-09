524041ad589b8f639100c58358c7bd678bace859
xiejie <imxiejie@gmail.com>
2022-11-09 21:46:42 +0800

    interface to any

--- web/internal/ginrsp/ginrsp.go
+++ web/internal/ginrsp/ginrsp.go
@@ -6,11 +6,11 @@ func Error(code int, msg string) gin.H {
 	return Custom(code, msg, gin.H{})
 }
 
-func Succ(msg string, data interface{}) gin.H {
+func Succ(msg string, data any) gin.H {
 	return Custom(200, msg, data)
 }
 
-func Custom(code int, msg string, data interface{}) gin.H {
+func Custom(code int, msg string, data any) gin.H {
 	return gin.H{
 		"code": code,
 		"msg":  msg,
